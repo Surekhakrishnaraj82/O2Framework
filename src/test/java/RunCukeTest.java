e81c10e63aab09a9d01eca7c3c3db3f5f2cd9e24
Surekhakrishnaraj82 <surekesh@gmail.com>
2019-10-12 18:36:35 +0100

    modification

--- src/test/java/RunCukeTest.java
+++ src/test/java/RunCukeTest.java
@@ -2,7 +2,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/")
+@CucumberOptions(features = "src/test/resources/", plugin = {"json:Target/cucumber.json"})
 public class RunCukeTest {
 
 }
